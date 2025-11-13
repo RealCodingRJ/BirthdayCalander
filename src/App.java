607f918c2382bc479bf655af790e53e62744deeb
Crawz <realcoder2023@gmail.com>
2025-11-13 09:21:17 -0500

    removed exit code

--- src/App.java
+++ src/App.java
@@ -2,17 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 class Window extends JFrame {
     public Window() {
 
+        super("Birthday Days Left");
         SwingUtilities.invokeLater(() -> {
 
-            setTitle("Birthday Days Left");
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             setSize(new Dimension(300, 600));
 
@@ -34,15 +32,8 @@ class Window extends JFrame {
 
 void main () {
 
- Window n = new Window();
- n.setVisible(true);
- n.addWindowListener(new WindowAdapter() {
-     @Override
-     public void windowOpened(WindowEvent e) {
-         super.windowOpened(e);
-         System.exit(0);
-     }
- });
+    Window n = new Window();
+    n.setVisible(true);
 }
 
 private JLabel getJLabel(int day, int Month) {
