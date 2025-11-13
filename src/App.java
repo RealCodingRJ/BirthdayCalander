ad9bf42eee6094723a7717aa18f93a4930fed1f6
Crawz <realcoder2023@gmail.com>
2025-11-13 09:05:36 -0500

    fixed background color not setting

--- src/App.java
+++ src/App.java
@@ -5,21 +5,42 @@ import java.awt.event.MouseEvent;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-void main() {
+class Window extends JFrame {
+    public Window() {
 
-    JFrame frame = new JFrame("Birthday Day Time Zone");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(new Dimension(300, 600));
+        SwingUtilities.invokeLater(() -> {
 
-    GregorianCalendar cale = new GregorianCalendar();
-    Dates d = new Dates();
-    int day = d.Sub(11, cale.get(Calendar.MONTH));
-    int Month = d.Sub(17, cale.get(Calendar.DATE));
+            setTitle("Birthday Days Left");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setSize(new Dimension(300, 600));
 
+            GregorianCalendar cale = new GregorianCalendar();
+            Dates d = new Dates();
+            int day = d.Sub(11, cale.get(Calendar.MONTH));
+            int Month = d.Sub(17, cale.get(Calendar.DATE));
+
+            JLabel label = getJLabel(day, Month);
+            setResizable(false);
+            getContentPane().setBackground(new Color(20, 20, 20));
+
+            label.setForeground(new Color(255, 255, 255));
+            add(label);
+
+        });
+    }
+}
+
+void main () {
+
+ Window n = new Window();
+ n.setVisible(true);
+}
+
+private JLabel getJLabel(int day, int Month) {
     var text = (day + 1) - Month + " Days";
     JLabel label = new JLabel("Your Birthday is in: " + text, SwingConstants.CENTER);
-    label.setForeground(new Color(255, 255, 255));
-    frame.getContentPane().setBackground(new Color(20, 20, 20));
+
+    JFrame frame = new JFrame("Birthday Days Left");
 
     if ((day + 1) - Month == 17) {
 
@@ -27,16 +48,12 @@ void main() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 label.setText("Happy Birthday");
-                frame.getContentPane().setBackground(new Color(255, 0,0));
+                frame.setBackground(new Color(255, 0, 0));
                 super.mouseClicked(e);
             }
         });
 
     }
 
-    frame.add(label, BorderLayout.CENTER);
-    frame.setResizable(false);
-    frame.setVisible(true);
-
+    return label;
 }
-
