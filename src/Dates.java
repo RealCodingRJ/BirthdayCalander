8f7045d6c96b1fac26fcea8e7c3ca5e9191aa07e
Crawz <realcoder2023@gmail.com>
2025-11-12 21:38:53 -0500

    removed some files

--- src/Dates.java
+++ src/Dates.java
@@ -1,12 +1,4 @@
-import java.time.LocalDate;
-
 public class Dates {
-
-    public LocalDate Dates() {
-
-        LocalDate d = LocalDate.now();
-        return d;
-    }
 
     public int Sub(int a, int b) {
         return b - a;
